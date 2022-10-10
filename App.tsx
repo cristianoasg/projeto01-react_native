4bc92a86a24a9d1f95d2c915f205b9a0807bdf9d
Cristiano Augusto <cristiano.asg@hotmail.com>
2022-10-10 23:01:37 +0100

    styling elements

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <>
-      <Text>React </Text>
-      <Text>native</Text>
-    </>
+    <View style={{
+      flex: 1,
+      backgroundColor: '#131016',
+      padding: 26
+    }}>
+      <Text style={{
+        color: '#FDFCFE',
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 48
+      }}>
+        Nome do evento
+      </Text>
+
+      <Text style={{
+        color: '#6B6B6B',
+        fontSize: 16,
+      }}>
+        Sexta, 4 de novembro de 2022.
+      </Text>
+    </View>
   )
 }
